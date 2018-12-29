7a24e378e8b4fb4db82cf53be5c2decd0386546f
albert1299 <46204563+albert1299@users.noreply.github.com>
2018-12-28 21:05:19 -0500

    modelo cuenta finalizado

---
+++ Models/cuenta.js
@@ -0,0 +1,16 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var CuentaSchema = Schema({
+    id: mongoose.Schema.Types.ObjectId,
+    external_id: String,
+    estado: {type: Boolean, default: true},
+    correo: String,
+    clave: String,
+    persona: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Persona"
+    }
+});
+
+module.exports = mongoose.model("Cuenta", CuentaSchema);
