52d666e236ab6828f88d20be46a1654e8a1aa5a5
JackAndrewG <jackson.guzman@unl.edu.ec.com>
2018-12-28 20:29:44 -0500

    1 implementacion de el modelo Ruta

--- Models/ruta.js
+++ Models/ruta.js
@@ -1,1 +1,13 @@
 'use strict';
+var mongoose =require('mongoose');
+var Schema = mongoose.Schema;
+var RutaSchema=Schema({
+  id: mongoose.Schema.Types.ObjectId,
+  external_id: String,
+  origen: String,
+  destino: String,
+  valor: Numeric,
+  estado: {type: Boolean, default: true}
+}, false);
+
+module.exports=mongoose.model('Ruta', RutaSchema);
