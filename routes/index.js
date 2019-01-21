cc82879ae251de85ef98f20036a832df4a0cbf0a
JackAndrewG <jackson.guzman@unl.edu.ec>
2019-01-20 21:21:08 -0500

    creacion de vistas y fragmentos

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { titulo: 'TransNIC Cooperativa' });
+  res.render('fragmentos/frmInicio', { titulo: 'TransNIC Cooperativa'});
 });
 
+/* Obtener Registro de Usuario */
+router.get('/registro', function(req, res, next) {
+  res.render('fragmentos/frmRegistro', { titulo: 'Registro de Usuario'});
+});
 module.exports = router;
