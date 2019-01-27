b6764025aceca12f36463cc1b06307325273d772
JackAndrewG <jackson.guzman@unl.edu.ec>
2019-01-27 00:51:21 -0500

    Construccion de vista de pagos (paypal) para usuarios

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,22 @@ router.get('/', function(req, res, next) {
   res.render('fragmentos/frmInicio', { titulo: 'TransNIC Cooperativa'});
 });
 
-/* Obtener Destinos Admin */
+/*RUTAS ADMINISTRADOR*/
+/* Obtener Destinos */
 router.get('/administrador/destinos', function(req, res, next) {
   res.render('fragmentos/vistaAdmin/frmDestino', { titulo: 'Administrar Destinos'});
 });
-/* Obtener Buses Admin */
+/* Obtener Buses */
 router.get('/administrador/buses', function(req, res, next) {
   res.render('fragmentos/vistaAdmin/frmBus', { titulo: 'Administrar Unidades de Transporte'});
 });
 
-/* Obtener Registro de Usuario */
+/*RUTAS USUARIO*/
+/* Obtener Compra */
+router.get('/comprar', function(req, res, next) {
+  res.render('fragmentos/vistaUsuario/frmCompra', { titulo: 'Compra de Boletos'});
+});
+/* Obtener Registro */
 router.get('/registro', function(req, res, next) {
   res.render('fragmentos/frmRegistro', { titulo: 'Registro de Usuario'});
 });
