b41ced9fba11099ea12c7d17b10c32bb30d5eca3
JackAndrewG <jackson.guzman@unl.edu.ec>
2019-01-26 23:37:19 -0500

    construccion de vista Destinos para administradores

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,11 @@ router.get('/', function(req, res, next) {
   res.render('fragmentos/frmInicio', { titulo: 'TransNIC Cooperativa'});
 });
 
+/* Obtener Destinos Admin */
+router.get('/administrador/destinos', function(req, res, next) {
+  res.render('fragmentos/vistaAdmin/frmDestino', { titulo: 'Administrar Destinos'});
+});
+
 /* Obtener Registro de Usuario */
 router.get('/registro', function(req, res, next) {
   res.render('fragmentos/frmRegistro', { titulo: 'Registro de Usuario'});
