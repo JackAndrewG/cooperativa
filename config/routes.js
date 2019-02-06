64e0f0849801e10d629f877c735054d75a552373
¨JackAndrewG¨ <jack_andres@outlook.com>
2019-02-06 14:13:07 -0500

    Correcciones...

--- config/routes.js
+++ config/routes.js
@@ -2,19 +2,14 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
-<<<<<<< HEAD
-
 var ruta = require('../controladores/rutaControlador');
 var rutaControlador = new ruta();
 
 var unidad = require('../controladores/unidadesControlador');
 var unidadesControlador = new unidad();
 
-=======
-//var unidades = require('../controladores/unidadesController');
-//var unidadesController = new unidades();
->>>>>>> 087074755897804b5b95d774f9079d8cefe6a9fe
-//solo dejara ver la diferentes secciones si ha iniciado sesion
+
+//Control de Sesion
 var auth = function middleWare(req, res, next) {
     if (req.isAuthenticated()) {
         next();
@@ -37,82 +32,20 @@ router.get('/', function(req, res, next) {
       }
 });
 
-/* Obtener Registro de Usuario */
-router.get('/registro', function(req, res, next) {
-  res.render('fragmentos/vistaUsuario/frmRegistro', {
-      titulo: 'Registro de Usuario',
-      error: req.flash("correo_repetido")
-  });
-});
-
-/* Obtener Login de Usuario */
-router.get('/inicio_sesion', function(req, res, next) {
-  res.render('fragmentos/vistaUsuario/frmLogin', {
-      titulo: 'Inicio de Sesion',
-      error: req.flash("err_cred")
-  });
-});
-
-// Registrar nuevo usuario
-
-
-
-router.post('/registro/guardar',
-        passport.authenticate('local-signup', {successRedirect: '/inicio_sesion',
-            failureRedirect: '/registro', failureFlash: true}
-        ));
-// registrar un nuevo bus
-//router.post('/administrador/buses/guardar', auth, UnidadesController.guardar);
-
-// Obtener Registro de Usuario
-
 /*RUTAS ADMINISTRADOR*/
+
 /* Obtener Destinos */
-<<<<<<< HEAD
-
 router.get('/destinos', auth, rutaControlador.verRutas);
-
 
 /*router.get('/destinos', auth, function(req, res, next) {
     //if (req.user.rol === "administrador") {} <- utilizar
-
-
  res.render('fragmentos/vistaAdmin/frmDestino', { titulo: 'Administrar Destinos',
  session: req.isAuthenticated()});
 }); */
-/* Obtener Buses */
-router.get('/administrador/buses', auth, unidadesControlador.verBuses);
-=======
-router.get('/administrador/destinos', auth, function(req, res, next) {
- res.render('fragmentos/vistaAdmin/frmEditar', { titulo: 'Administrar Destinos'});
-});
-/* Obtener Buses */
-/*
-router.get('/administrador/buses', auth, function(req, res, next) {
- res.render('fragmentos/vistaAdmin/frmBus', { titulo: 'Administrar Unidades de Transporte'});
-});
->>>>>>> 087074755897804b5b95d774f9079d8cefe6a9fe
-
-*/
-
-router.get('/administrador/buses', auth, function(req, res, next) {
-     res.render('fragmentos/vistaAdmin/frmBus', { titulo: 'Administrar Unidades de Transporte'});
-    });
-
-/*
-router.get('/usuario/busesi', auth, function(req, res, next) {
-   res.render('fragmentos/vistaUsuario/frmVistaUnidades', { titulo: 'Administrar Unidades de Transporte'});
-  });
-*/
-
 
 
 /*RUTAS USUARIO*/
-/* Obtener Destinos */
-/*
-router.get('/destinos', auth, function(req, res, next) {
- res.render('fragmentos/vistaUsuario/frmDestino', { titulo: 'Destinos de Viaje'});
-}); */
+
 /* Obtener Contactenos */
 router.get('/contactenos', auth, function(req, res, next) {
  res.render('fragmentos/vistaUsuario/frmContactenos', { titulo: 'Contactenos',
@@ -122,8 +55,6 @@ session: req.isAuthenticated()});
 router.get('/comprar', auth, function(req, res, next) {
  res.render('fragmentos/vistaUsuario/frmCompra', { titulo: 'Compra de Boletos'});
 });
-
-//probando atom master
 
 //para iniciar Sesion
 router.post('/inicio_sesion/iniciar',
@@ -142,18 +73,43 @@ router.get('/inicio', auth, function(req, res, next) {
          });
         });
 
+        /* Obtener Registro de Usuario */
+        router.get('/registro', function(req, res, next) {
+          res.render('fragmentos/vistaUsuario/frmRegistro', {
+              titulo: 'Registro de Usuario',
+              error: req.flash("correo_repetido")
+          });
+        });
+
+        /* Obtener Login de Usuario */
+        router.get('/inicio_sesion', function(req, res, next) {
+          res.render('fragmentos/vistaUsuario/frmLogin', {
+              titulo: 'Inicio de Sesion',
+              error: req.flash("err_cred")
+          });
+        });
+
+        // Registrar nuevo usuario
+        router.post('/registro/guardar',
+                passport.authenticate('local-signup', {successRedirect: '/inicio_sesion',
+                    failureRedirect: '/registro', failureFlash: true}
+                ));
+
 //cerrar sesion
 router.get('/cerrar_sesion', function(req, res, next){
   req.session.destroy();
   res.redirect('/');
 });
 
+
+// FRECUENCIAS
 //registro de frecuencias
 router.post('/guardar_destino', auth, rutaControlador.guardar);
 
-//registro de buses
-router.post('/guardar_bus', auth, unidadesControlador.guardar);
 
+/* BUSES */
+router.post('/guardar_bus', auth, unidadesControlador.guardar);
+router.get('/administrador/buses', auth, unidadesControlador.verBuses);
 
 
 
