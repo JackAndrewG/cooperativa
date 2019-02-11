2f6f9a28b83a8bb7b61a21a8cecf0bff2aedbbc0
JackAndrewG <jackson.guzman@unl.edu.ec>
2019-02-11 13:40:33 -0500

    Editar Buses, notificaciones flash y html5

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-
-
 var ruta = require('../controladores/rutaControlador');
 var rutaControlador = new ruta();
 
@@ -10,21 +8,7 @@ var unidad = require('../controladores/unidadesControlador');
 var unidadesControlador = new unidad();
 
 
-
-
-
-//Control de Sesion
-
-var auth = function middleWare(req, res, next) {
-    if (req.isAuthenticated()) {
-        next();
-    } else {
-        req.flash('err_cred', 'Inicia sesion!!!');
-        res.redirect('/inicio_sesion');
-    }
-}
-
-/* Obtener Pagina Principal. */
+/*PAGINA PRINCIPAL*/
 // presenta plantilla segun esta iniciada la sesion o no
 router.get('/', function(req, res, next) {
   if (req.isAuthenticated()) {
@@ -37,6 +21,18 @@ router.get('/', function(req, res, next) {
       }
 });
 
+//CONTROL DE SESION
+var auth = function middleWare(req, res, next) {
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        req.flash('err_cred', '¡Inicia sesion!');
+        res.redirect('/inicio_sesion');
+    }
+}
+
+
+/*REGISTRO*/
 
 /* Obtener Registro de Usuario */
 router.get('/registro', function(req, res, next) {
@@ -45,56 +41,56 @@ router.get('/registro', function(req, res, next) {
       error: req.flash("correo_repetido")
   });
 });
-
-/* Obtener Login de Usuario */
-router.get('/inicio_sesion', function(req, res, next) {
-  res.render('fragmentos/vistaUsuario/frmLogin', {
-      titulo: 'Inicio de Sesion',
-      error: req.flash("err_cred")
-  });
-});
-
 // Registrar nuevo usuario
 router.post('/registro/guardar',
         passport.authenticate('local-signup', {successRedirect: '/inicio_sesion',
             failureRedirect: '/registro', failureFlash: true}
         ));
 
-// Obtener Registro de Usuario
 
-/*RUTAS ADMINISTRADOR*/
-/* Obtener Destinos */
+/* INICIO DE SESION */
+router.get('/inicio_sesion', function(req, res, next) {
+  res.render('fragmentos/vistaUsuario/frmLogin', {
+      titulo: 'Inicio de Sesion',
+      error: req.flash("err_cred")
+  });
+});
+        //para iniciar Sesion
+        router.post('/inicio_sesion/iniciar',
+                passport.authenticate('local-signin',
+                        {successRedirect: '/inicio',
+                            failureRedirect: '/inicio_sesion',
+                            failureFlash: true}
+                ));
+
+        //en caso de que ya haya iniciado sesion presentará una nueva vista
+        router.get('/inicio', auth, function(req, res, next) {
+                 res.render('fragmentos/plantilla_sesion_iniciada', {
+                   titulo: 'Bienvenido',
+                   usuario: req.user.nombre,
+                   session: req.isAuthenticated()
+                 });
+                });
+
+        //cerrar sesion
+        router.get('/cerrar_sesion', function(req, res, next){
+          req.session.destroy();
+          res.redirect('/');
+        });
 
 
-/*RUTAS ADMINISTRADOR*/
-
-/* Obtener Destinos */
-
+                                                      /*RUTAS ADMINISTRADOR*/
+/* DESTINOS */
 router.get('/destinos', auth, rutaControlador.verRutas);
 
-/*router.get('/destinos', auth, function(req, res, next) {
-    //if (req.user.rol === "administrador") {} <- utilizar
- res.render('fragmentos/vistaAdmin/frmDestino', { titulo: 'Administrar Destinos',
- session: req.isAuthenticated()});
-}); */
+router.post('/guardar_destino', auth, rutaControlador.guardar);
+router.post('/editarDestino', auth, rutaControlador.editar);
 
-/*router.get('/destinos', auth, function(req, res, next) {
-    //if (req.user.rol === "administrador") {} <- utilizar
-
-
- res.render('fragmentos/vistaAdmin/frmDestino', { titulo: 'Administrar Destinos',
- session: req.isAuthenticated()});
-}); */
-/* Obtener Buses */
+/* BUSES */
 router.get('/administrador/buses', auth, unidadesControlador.verBuses);
 
-/*RUTAS USUARIO*/
-
-/* Obtener Destinos */
-/*
-router.get('/destinos', auth, function(req, res, next) {
- res.render('fragmentos/vistaUsuario/frmDestino', { titulo: 'Destinos de Viaje'});
-}); */
+router.post('/guardar_bus', auth, unidadesControlador.guardar);
+router.post('/editarBus', auth, unidadesControlador.editar);
 
 
 
@@ -108,67 +104,34 @@ router.get('/comprar', auth, function(req, res, next) {
  res.render('fragmentos/vistaUsuario/frmCompra', { titulo: 'Compra de Boletos'});
 });
 
-//para iniciar Sesion
-router.post('/inicio_sesion/iniciar',
-        passport.authenticate('local-signin',
-                {successRedirect: '/inicio',
-                    failureRedirect: '/inicio_sesion',
-                    failureFlash: true}
-        ));
-
-//en caso de que ya haya iniciado sesion presentará una nueva vista
-router.get('/inicio', auth, function(req, res, next) {
-         res.render('fragmentos/plantilla_sesion_iniciada', {
-           titulo: 'Bienvenido',
-           usuario: req.user.nombre,
-           session: req.isAuthenticated()
-         });
-        });
 
 
-        /* Obtener Registro de Usuario */
-        router.get('/registro', function(req, res, next) {
-          res.render('fragmentos/vistaUsuario/frmRegistro', {
-              titulo: 'Registro de Usuario',
-              error: req.flash("correo_repetido")
-          });
-        });
-
-        /* Obtener Login de Usuario */
-        router.get('/inicio_sesion', function(req, res, next) {
-          res.render('fragmentos/vistaUsuario/frmLogin', {
-              titulo: 'Inicio de Sesion',
-              error: req.flash("err_cred")
-          });
-        });
-
-        // Registrar nuevo usuario
-        router.post('/registro/guardar',
-                passport.authenticate('local-signup', {successRedirect: '/inicio_sesion',
-                    failureRedirect: '/registro', failureFlash: true}
-                ));
 
 
-//cerrar sesion
-router.get('/cerrar_sesion', function(req, res, next){
-  req.session.destroy();
-  res.redirect('/');
-});
-
-//registro de frecuencias
-router.post('/guardar_destino', auth, rutaControlador.guardar);
-
-//registro de buses
-router.post('/guardar_bus', auth, unidadesControlador.guardar);
 
 
-/* DESTINOS */
-router.post('/guardar_destino', auth, rutaControlador.guardar);
-router.post('/editarDestino', auth, rutaControlador.editar);
+/*RUTAS ADMINISTRADOR*/
+/*router.get('/destinos', auth, function(req, res, next) {
+    //if (req.user.rol === "administrador") {} <- utilizar
+ res.render('fragmentos/vistaAdmin/frmDestino', { titulo: 'Administrar Destinos',
+ session: req.isAuthenticated()});
+}); */
 
-/* BUSES */
-router.post('/guardar_bus', auth, unidadesControlador.guardar);
-router.get('/administrador/buses', auth, unidadesControlador.verBuses);
+/*router.get('/destinos', auth, function(req, res, next) {
+    //if (req.user.rol === "administrador") {} <- utilizar
+
+
+ res.render('fragmentos/vistaAdmin/frmDestino', { titulo: 'Administrar Destinos',
+ session: req.isAuthenticated()});
+}); */
+
+/*RUTAS USUARIO*/
+
+/* Obtener Destinos */
+/*
+router.get('/destinos', auth, function(req, res, next) {
+ res.render('fragmentos/vistaUsuario/frmDestino', { titulo: 'Destinos de Viaje'});
+}); */
 
 
 
--- controladores/unidadesControlador.js
+++ controladores/unidadesControlador.js
@@ -4,18 +4,47 @@ var Bus = models.bus;
 const uuidv4 = require('uuid/v4');
 class unidadesControlador {
 
-    guardar(req, res) {
-        Bus.create({
-            external_id: uuidv4(),
-            numeroBus: req.body.numerobus,
-            placa: req.body.placa,
-            propietario: req.body.propietario,
-            numeroAsientos: req.body.numeroasientos
-        }).then(function (newBus, created) {
-            if (newBus) {
-                req.flash('info', 'Se ha creado correctamente');
-                res.redirect('/administrador/buses');
+    guardar(req, res, message) {
+      Bus.findOne({where: {numeroBus: req.body.numerobus}}).then(function (existeN, placa) {
+        if (existeN) {
+          req.flash('alerta', 'ERROR AL GUARDAR, EL NUMERO DE BUS YA EXISTE');
+          res.redirect('/administrador/buses');
+        }else{
+          Bus.findOne({where: {placa: req.body.placa}}).then(function (existeP, nuevo) {
+            if (existeP) {
+              req.flash('alerta', 'ERROR AL GUARDAR, LA PLACA YA ESTA REGISTRADA');
+              res.redirect('/administrador/buses');
+            }else{
+                        Bus.create({
+                            external_id: uuidv4(),
+                            numeroBus: req.body.numerobus,
+                            placa: req.body.placa,
+                            propietario: req.body.propietario,
+                            numeroAsientos: req.body.numeroasientos
+                        }).then(function (newBus, err) {
+                            if (newBus) {
+                                req.flash('exito', 'DATOS GUARDADOS CORRECTAMENTE');
+                            }
+                            res.redirect('/administrador/buses');
+                        });
             }
+          });
+        }
+      });
+    }
+
+    editar(req, res, done) {
+
+        Bus.update({
+          numeroBus: req.body.numerobus,
+          placa: req.body.placa,
+          propietario: req.body.propietario,
+          numeroAsientos: req.body.numeroasientos
+        }, {where: {external_id: req.body.external}}).then(function (editado, err) {
+            if (editado) {
+                        req.flash('alerta', 'DATOS MODIFICADOS CORRECTAMENTE');
+                        res.redirect('/administrador/buses');
+                    }
         });
     }
 
@@ -30,8 +59,9 @@ class unidadesControlador {
 
 
                         buses: buses,
-                        session: req.isAuthenticated()
-
+                        session: req.isAuthenticated(),
+                        mensaje: req.flash("alerta"),
+                        exito: req.flash("exito")
                                 //info: (req.flash('info') != '') ? req.flash('info') : '',
                                 //error: (req.flash('error') != '') ? req.flash('error') : ''
                     });
@@ -48,5 +78,3 @@ class unidadesControlador {
 
 
 module.exports = unidadesControlador;
-
-
